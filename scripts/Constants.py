fc590c1b18dec183c51b3569f86c71fc2355f862
Ishita Nigam <79245974+Ishita2015@users.noreply.github.com>
2021-08-30 16:07:40 +0530

    Update Constants.py

--- scripts/Constants.py
+++ scripts/Constants.py
@@ -1,11 +1,11 @@
 from requests.auth import HTTPBasicAuth
 
 TEST_END_POINT = "https://ishitanigam.atlassian.net/rest/api/2/issue/"
-# GET_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue/APIAUTO-1"
-# POST_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue"
-# PUT_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue/APIAUTO-8"
-# DELETE_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue/APIAUTO-5"
-AUTH = HTTPBasicAuth("ishita.nigam@outlook.com", "GWqitVShMZctbXrFdluaCF1A")
+GET_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue/APIAUTO-1"
+POST_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue"
+PUT_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue/APIAUTO-8"
+DELETE_URL = "https://ishitanigam.atlassian.net/rest/api/2/issue/APIAUTO-5"
+AUTH = HTTPBasicAuth(os.environ.get('rest_api_user'), os.environ.get('rest_api_key'))
 DEFAULT_HEADERS = {
     "Accept": "application/json",
     "Connection": "keep-alive",
